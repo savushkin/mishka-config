404ba636c5fb2a848530d22e38d8b4e619dbf1cd
Ivan Savushkin <ivansavuskin@gmail.com>
2017-10-30 15:52:45 +0300

    untar

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,3 +1,4 @@
 alias video_laptop="xrandr --output eDP-1 --mode 1600x900 --pos 0x0 --output VGA-1 --off"
 alias video_h750="xrandr --output VGA-1 --mode 1280x1024 --pos 0x0 --output eDP-1 --mode 1600x900 --pos 1280x0"
 alias apt="sudo apt"
+alias untar="tar xvzf"
