2735379eda2e588e2051733f8b14d5660100950f
Ivan Savushkin <ivansavuskin@gmail.com>
2017-10-30 14:57:12 +0300

    bash files

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-rm ~/.config/i3/config
-ln -s ~/.config/mishka-config/i3.conf ~/.config/i3/config
-rm ~/.gitconfig
-ln -s ~/.config/mishka-config/.gitconfig ~/.gitconfig
-rm ~/.i3status.conf
+CONFIG_PATH="/home/ivan/.config/mishka-config"
+rm ~/.config/i3/config ~/.gitconfig ~/.bashrc ~/.bash_aliases ~/.i3status.conf
+ln -s ${CONFIG_PATH}/i3.conf ~/.config/i3/config
+ln -s ${CONFIG_PATH}/.gitconfig ~/.gitconfig
+ln -s ${CONFIG_PATH}/bashrc.sh ~/.bashrc
+ln -s ${CONFIG_PATH}/bash_aliases.sh ~/.bash_aliases
 case $1 in
     -l|--laptop)
-    ln -s ~/.config/mishka-config/i3status.conf ~/.i3status.conf
+    ln -s ${CONFIG_PATH}/i3status.conf ~/.i3status.conf
     ;;
     -d|--desktop)
-    ln -s ~/.config/mishka-config/i3status-desktop.conf ~/.i3status.conf
+    ln -s ${CONFIG_PATH}/i3status-desktop.conf ~/.i3status.conf
     ;;
 esac
